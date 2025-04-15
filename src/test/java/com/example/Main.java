7759d67c3a8d17132977ee56d67f65d50c684b5a
dafebj <daphinefebij@gmail.com>
2025-04-15 12:38:14 +0530

    commit

---
+++ src/test/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Test");
+        System.out.println("User's Name: " + user.getName());
+    }
+}
+
